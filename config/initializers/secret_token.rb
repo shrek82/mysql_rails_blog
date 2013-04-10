9ae145f864f1e9a3f447bcd5c4cbae19e92dbad4
shrek82 <37294812@qq.com>
2013-04-10 23:46:26 +0800

    初始化rails mysql blog project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysqlblog::Application.config.secret_token = '6b2ebe05cd4263e18ccc890ba3fd1032085930419240ead1fb7671d6f78607284f76971aeb46ca3c53f18e1ae7d600250095c5060d3fb7a106ea1acca047bf18'
