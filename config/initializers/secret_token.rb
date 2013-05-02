574ea0f745d6566fdbf30687777b9d49a06482cf
shrek82 <37294812@qq.com>
2013-05-03 06:39:07 +0800

    上传项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tblog::Application.config.secret_token = 'af57c23eb9952b02bd24e78fffea183df9e8d1d5e6183680cfe735d126630be15161abcbd175db009598c65722ee638431ffeb91a2f8b0ffafd68d8e9e14a56e'
